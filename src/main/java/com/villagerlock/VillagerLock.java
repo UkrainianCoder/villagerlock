56d2fc4f65acdec7661180707fb3f083023da628
UkrainianCoder <111013469+UkrainianCoder@users.noreply.github.com>
2026-03-01 14:44:25 +0200

    feat(main, client): add hello world code with grandlew options

---
+++ src/main/java/com/villagerlock/VillagerLock.java
@@ -0,0 +1,16 @@
+package com.villagerlock;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class VillagerLock implements ModInitializer {
+	public static final String MOD_ID = "villager-lock";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
