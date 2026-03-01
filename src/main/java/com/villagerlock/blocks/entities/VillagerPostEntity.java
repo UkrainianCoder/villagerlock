243089ecc4c1eceac52bd9a760b471e600ca75f1
UkrainianCoder <111013469+UkrainianCoder@users.noreply.github.com>
2026-03-01 17:19:09 +0200

    feat(main/java): villager post block entity with seat / unseat features

---
+++ src/main/java/com/villagerlock/blocks/entities/VillagerPostEntity.java
@@ -0,0 +1,80 @@
+package com.villagerlock.blocks.entities;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.decoration.ArmorStandEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+import static com.villagerlock.ModBlocks.VILLAGER_POST_ENTITY;
+
+public class VillagerPostEntity extends BlockEntity {
+	private int entityId = -1;
+	private int seatId = -1;
+
+	public VillagerPostEntity(BlockPos pos, BlockState state) {
+		super(VILLAGER_POST_ENTITY, pos, state);
+	}
+
+	public static void onTick(World world, BlockPos pos, BlockState state, VillagerPostEntity blockEntity) {
+		if (blockEntity.isOccupied()) {
+			boolean kill = false;
+			Entity rider = world.getEntityById(blockEntity.getEntityId());
+			Entity seat = world.getEntityById(blockEntity.getSeatId());
+
+			if (rider == null || rider.getVehicle() != seat) {
+				kill = true;
+			}
+
+			if (seat == null) {
+				kill = true;
+			}
+
+			if (kill) {
+				blockEntity.unseat(world);
+			}
+		}
+	}
+
+	public boolean isOccupied() {
+		return entityId != -1 && seatId != -1;
+	}
+
+	public int getEntityId() {
+		return entityId;
+	}
+
+	public int getSeatId() {
+		return seatId;
+	}
+
+	public void seat(World world, Entity entity) {
+		ArmorStandEntity seat = new ArmorStandEntity(world, pos.getX() + 0.5, pos.getY() - 2, pos.getZ() + 0.5);
+		seat.setInvisible(true);
+		seat.setNoGravity(true);
+		world.spawnEntity(seat);
+		entity.startRiding(seat);
+		this.entityId = entity.getId();
+		this.seatId = seat.getId();
+	}
+
+	public void unseat(World world) {
+		if (seatId != -1 && entityId != -1) {
+			Entity seat = world.getEntityById(seatId);
+			Entity rider = world.getEntityById(entityId);
+
+			if (seat != null) {
+				seat.remove(Entity.RemovalReason.DISCARDED);
+			}
+
+			if (rider != null && rider.getVehicle() == seat) {
+				rider.stopRiding();
+			}
+
+			this.entityId = -1;
+			this.seatId = -1;
+		}
+	}
+}
+
