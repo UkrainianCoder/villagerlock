99c8ed1211e79694d54bf43dce4ea2a8efb431d5
UkrainianCoder <111013469+UkrainianCoder@users.noreply.github.com>
2026-03-01 17:19:31 +0200

    feat(main/java): use custom entity on villiager post block

--- src/main/java/com/villagerlock/blocks/VillagerPost.java
+++ src/main/java/com/villagerlock/blocks/VillagerPost.java
@@ -1,16 +1,29 @@
 package com.villagerlock.blocks;
 
-import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.ShapeContext;
+import com.mojang.serialization.MapCodec;
+import com.villagerlock.ModBlocks;
+import com.villagerlock.blocks.entities.VillagerPostEntity;
+import net.minecraft.block.*;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityTicker;
+import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityCollisionHandler;
+import net.minecraft.entity.passive.VillagerEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.resource.featuretoggle.FeatureSet;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockRenderView;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
+import net.minecraft.world.event.listener.GameEventListener;
+import org.jspecify.annotations.Nullable;
 
-public class VillagerPost extends Block {
+public class VillagerPost extends BlockWithEntity implements BlockEntityProvider {
 	private static final VoxelShape SHAPE;
 
 	static {
@@ -36,12 +49,66 @@ public class VillagerPost extends Block {
 	}
 
 	@Override
-	protected VoxelShape getCollisionShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
-		return SHAPE;
+	public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
+		return new VillagerPostEntity(pos, state);
 	}
 
 	@Override
-	protected VoxelShape getInsideCollisionShape(BlockState state, BlockView world, BlockPos pos, Entity entity) {
-		return SHAPE;
+	public <T extends BlockEntity> BlockEntityTicker<T> getTicker(World world, BlockState state, BlockEntityType<T> type) {
+		if (!world.isClient() && type == ModBlocks.VILLAGER_POST_ENTITY) {
+			return (w, pos, s, be) -> {
+				if (be instanceof VillagerPostEntity post) {
+					VillagerPostEntity.onTick(w, pos, s, post);
+				}
+			};
+		}
+
+		return super.getTicker(world, state, type);
+	}
+
+	@Override
+	public BlockState onBreak(World world, BlockPos pos, BlockState state, PlayerEntity player) {
+		BlockEntity blockEntity = world.getBlockEntity(pos);
+		if (blockEntity instanceof VillagerPostEntity postBlockEntity) {
+			postBlockEntity.unseat(world);
+		}
+
+		return super.onBreak(world, pos, state, player);
+	}
+
+	@Override
+	protected void onEntityCollision(BlockState state, World world, BlockPos pos, Entity entity, EntityCollisionHandler handler, boolean bool) {
+		if (world.isClient()) {
+			return;
+		}
+
+		BlockEntity blockEntity = world.getBlockEntity(pos);
+		if (!(blockEntity instanceof VillagerPostEntity postBlockEntity)) {
+			return;
+		}
+
+		if (!postBlockEntity.isOccupied() && entity.getVehicle() == null && entity instanceof VillagerEntity) {
+			postBlockEntity.seat(world, entity);
+		}
+	}
+
+	@Override
+	protected MapCodec<? extends BlockWithEntity> getCodec() {
+		return null;
+	}
+
+	@Override
+	public @Nullable <T extends BlockEntity> GameEventListener getGameEventListener(ServerWorld world, T blockEntity) {
+		return super.getGameEventListener(world, blockEntity);
+	}
+
+	@Override
+	public BlockState getAppearance(BlockState state, BlockRenderView renderView, BlockPos pos, Direction side, @Nullable BlockState sourceState, @Nullable BlockPos sourcePos) {
+		return super.getAppearance(state, renderView, pos, side, sourceState, sourcePos);
+	}
+
+	@Override
+	public boolean isEnabled(FeatureSet enabledFeatures) {
+		return super.isEnabled(enabledFeatures);
 	}
 }
