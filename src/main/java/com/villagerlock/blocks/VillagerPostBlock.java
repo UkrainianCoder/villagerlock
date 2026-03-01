1dfa8b238d68adb471be400af8d1f245f74e845b
UkrainianCoder <111013469+UkrainianCoder@users.noreply.github.com>
2026-03-01 20:32:36 +0200

    feat(blocks): support waterlogged block, resolve #6

--- src/main/java/com/villagerlock/blocks/VillagerPostBlock.java
+++ src/main/java/com/villagerlock/blocks/VillagerPostBlock.java
@@ -12,20 +12,27 @@ import net.minecraft.entity.EntityCollisionHandler;
 import net.minecraft.entity.mob.ZombieEntity;
 import net.minecraft.entity.passive.VillagerEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
 import net.minecraft.resource.featuretoggle.FeatureSet;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockRenderView;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldView;
 import net.minecraft.world.block.WireOrientation;
 import net.minecraft.world.event.listener.GameEventListener;
+import net.minecraft.world.tick.ScheduledTickView;
 import org.jspecify.annotations.Nullable;
 
-public class VillagerPostBlock extends BlockWithEntity implements BlockEntityProvider {
+public class VillagerPostBlock extends BlockWithEntity implements BlockEntityProvider, Waterloggable {
 	private static final VoxelShape SHAPE;
 
 	static {
@@ -43,6 +50,12 @@ public class VillagerPostBlock extends BlockWithEntity implements BlockEntityPro
 
 	public VillagerPostBlock(AbstractBlock.Settings settings) {
 		super(settings);
+		this.setDefaultState(this.stateManager.getDefaultState().with(Properties.WATERLOGGED, false));
+	}
+
+	@Override
+	protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+		builder.add(Properties.WATERLOGGED);
 	}
 
 	@Override
@@ -102,6 +115,20 @@ public class VillagerPostBlock extends BlockWithEntity implements BlockEntityPro
 				post.unseat(world);
 			}
 		}
+	}
+
+	@Override
+	public FluidState getFluidState(BlockState state) {
+		return state.get(Properties.WATERLOGGED) ? Fluids.WATER.getStill(false) : super.getFluidState(state);
+	}
+
+	@Override
+	public BlockState getStateForNeighborUpdate(BlockState state, WorldView world, ScheduledTickView tickView, BlockPos pos, Direction direction, BlockPos neighborPos, BlockState neighborState, Random random) {
+		if (state.get(Properties.WATERLOGGED)) {
+			tickView.scheduleFluidTick(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
+		}
+
+		return super.getStateForNeighborUpdate(state, world, tickView, pos, direction, neighborPos, neighborState, random);
 	}
 
 	@Override
