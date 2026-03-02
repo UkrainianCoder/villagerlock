3e048de6d0eadf132311f07ac7657a692e245ec1
UkrainianCoder <111013469+UkrainianCoder@users.noreply.github.com>
2026-03-02 23:25:28 +0200

    feat(resources, mixin): automatic unseat if a villager converts into a zombie

---
+++ src/main/java/com/villagerlock/mixin/MobEntityMixin.java
@@ -0,0 +1,28 @@
+package com.villagerlock.mixin;
+
+import com.villagerlock.blocks.entities.VillagerPostBlockEntity;
+import com.villagerlock.blocks.helpers.VillagerPostBlockHelper;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.passive.VillagerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(MobEntity.class)
+public abstract class MobEntityMixin {
+	@Inject(method = "convertTo*", at = @At("HEAD"))
+	private void onConverted(CallbackInfoReturnable<MobEntity> cir) {
+		MobEntity oldEntity = (MobEntity) (Object) this;
+		if (oldEntity.getEntityWorld().isClient()) {
+			return;
+		}
+
+		if (oldEntity instanceof VillagerEntity oldVillager) {
+			VillagerPostBlockEntity entity = VillagerPostBlockHelper.getVillagerPostEntity(oldVillager);
+			if (entity != null && entity.isOccupied() && entity.getEntityUuid().equals(oldVillager.getUuid())) {
+				entity.unseat(oldEntity.getEntityWorld(), false);
+			}
+		}
+	}
+}
