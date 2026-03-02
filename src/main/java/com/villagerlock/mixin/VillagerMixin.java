a47aec91968c70e47a54260eb53cacedbb6ce45c
UkrainianCoder <111013469+UkrainianCoder@users.noreply.github.com>
2026-03-02 12:00:02 +0200

    feat(mixin): register task for assigning profession

--- src/main/java/com/villagerlock/mixin/VillagerMixin.java
+++ src/main/java/com/villagerlock/mixin/VillagerMixin.java
@@ -1,51 +1,27 @@
 package com.villagerlock.mixin;
 
-import com.villagerlock.blocks.entities.VillagerPostBlockEntity;
-import net.minecraft.block.entity.BlockEntity;
+import com.google.common.collect.ImmutableList;
+import com.mojang.datafixers.util.Pair;
+import com.villagerlock.tasks.AssignProfessionOnVillagePostTask;
+import net.minecraft.entity.ai.brain.task.Task;
+import net.minecraft.entity.ai.brain.task.VillagerTaskListProvider;
 import net.minecraft.entity.passive.VillagerEntity;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
-import org.slf4j.Logger;
-import org.spongepowered.asm.mixin.Final;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.village.VillagerProfession;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(VillagerEntity.class)
+import java.util.ArrayList;
+import java.util.List;
+
+@Mixin(VillagerTaskListProvider.class)
 public class VillagerMixin {
-	@Shadow
-	@Final
-	private static Logger LOGGER;
-
-	@Unique
-	private VillagerPostBlockEntity getVillagerPostEntity(VillagerEntity villager) {
-		World world = villager.getEntityWorld();
-		BlockPos pos = villager.getBlockPos();
-		BlockEntity blockEntity = world.getBlockEntity(pos);
-
-		if (blockEntity instanceof VillagerPostBlockEntity post && post.isOccupied() && post.getEntityUuid() == villager.getUuid()) {
-			return post;
-		}
-
-		BlockPos belowPos = pos.down();
-		BlockEntity belowBlockEntity = world.getBlockEntity(belowPos);
-		if (belowBlockEntity instanceof VillagerPostBlockEntity post && post.isOccupied() && post.getEntityUuid() == villager.getUuid()) {
-			return post;
-		}
-
-		return null;
-	}
-
-	@Inject(method = "tick", at = @At("TAIL"))
-	private void onTick(CallbackInfo ci) {
-		VillagerEntity villager = (VillagerEntity) (Object) this;
-		VillagerPostBlockEntity post = getVillagerPostEntity(villager);
-
-		if (post != null) {
-			LOGGER.info("Villager post block entity tick -> {} <- {}", post.getEntityUuid(), villager.getUuid());
-		}
+	@Inject(method = "createIdleTasks", at = @At("RETURN"), cancellable = true)
+	private static void injectMyTask(RegistryEntry<VillagerProfession> registryEntry, float speed, CallbackInfoReturnable<ImmutableList<Pair<Integer, ? extends Task<? super VillagerEntity>>>> cir) {
+		List<Pair<Integer, ? extends Task<? super VillagerEntity>>> tasks = new ArrayList<>(cir.getReturnValue());
+		tasks.add(Pair.of(2, new AssignProfessionOnVillagePostTask()));
+		cir.setReturnValue(ImmutableList.copyOf(tasks));
 	}
 }
