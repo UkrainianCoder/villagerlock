99e558311e651a81d9f333ed61d2d26ee81b6f8d
UkrainianCoder <111013469+UkrainianCoder@users.noreply.github.com>
2026-03-02 13:29:03 +0200

    feat: add oak variants to vialliger post

--- src/main/java/com/villagerlock/ModBlocks.java
+++ src/main/java/com/villagerlock/ModBlocks.java
@@ -24,9 +24,50 @@ import java.util.function.Function;
 import static com.villagerlock.VillagerLock.LOGGER;
 
 public class ModBlocks {
+	public static final Identifier VILLAGER_POST_ENTITY_ID = Identifier.of(VillagerLock.MOD_ID, "villagerpost_entity");
 	public static final Identifier VILLAGER_POST_ID = Identifier.of(VillagerLock.MOD_ID, "villagerpost");
 	public static final Block VILLAGER_POST = register(VILLAGER_POST_ID, VillagerPostBlock::new, createVilliagerPostSettings());
 	public static final Item VILLAGER_POST_ITEM = register(VILLAGER_POST_ID, ItemGroups.FUNCTIONAL, VILLAGER_POST, new Item.Settings());
+
+	public static final Identifier VILLAGER_POST_ACACIA_ID = Identifier.of(VillagerLock.MOD_ID, "villagerpost_acacia");
+	public static final Block VILLAGER_POST_ACACIA = register(VILLAGER_POST_ACACIA_ID, VillagerPostBlock::new, createVilliagerPostSettings());
+	public static final Item VILLAGER_POST_ACACIA_ITEM = register(VILLAGER_POST_ACACIA_ID, ItemGroups.FUNCTIONAL, VILLAGER_POST_ACACIA, new Item.Settings());
+
+	public static final Identifier VILLAGER_POST_BIRCH_ID = Identifier.of(VillagerLock.MOD_ID, "villagerpost_birch");
+	public static final Block VILLAGER_POST_BIRCH = register(VILLAGER_POST_BIRCH_ID, VillagerPostBlock::new, createVilliagerPostSettings());
+	public static final Item VILLAGER_POST_BIRCH_ITEM = register(VILLAGER_POST_BIRCH_ID, ItemGroups.FUNCTIONAL, VILLAGER_POST_BIRCH, new Item.Settings());
+
+	public static final Identifier VILLAGER_POST_CHERRY_ID = Identifier.of(VillagerLock.MOD_ID, "villagerpost_cherry");
+	public static final Block VILLAGER_POST_CHERRY = register(VILLAGER_POST_CHERRY_ID, VillagerPostBlock::new, createVilliagerPostSettings());
+	public static final Item VILLAGER_POST_CHERRY_ITEM = register(VILLAGER_POST_CHERRY_ID, ItemGroups.FUNCTIONAL, VILLAGER_POST_CHERRY, new Item.Settings());
+
+	public static final Identifier VILLAGER_POST_CRIMSON_ID = Identifier.of(VillagerLock.MOD_ID, "villagerpost_crimson");
+	public static final Block VILLAGER_POST_CRIMSON = register(VILLAGER_POST_CRIMSON_ID, VillagerPostBlock::new, createVilliagerPostSettings());
+	public static final Item VILLAGER_POST_CRIMSON_ITEM = register(VILLAGER_POST_CRIMSON_ID, ItemGroups.FUNCTIONAL, VILLAGER_POST_CRIMSON, new Item.Settings());
+
+	public static final Identifier VILLAGER_POST_DARK_OAK_ID = Identifier.of(VillagerLock.MOD_ID, "villagerpost_dark_oak");
+	public static final Block VILLAGER_POST_DARK_OAK = register(VILLAGER_POST_DARK_OAK_ID, VillagerPostBlock::new, createVilliagerPostSettings());
+	public static final Item VILLAGER_POST_DARK_OAK_ITEM = register(VILLAGER_POST_DARK_OAK_ID, ItemGroups.FUNCTIONAL, VILLAGER_POST_DARK_OAK, new Item.Settings());
+
+	public static final Identifier VILLAGER_POST_JUNGLE_ID = Identifier.of(VillagerLock.MOD_ID, "villagerpost_jungle");
+	public static final Block VILLAGER_POST_JUNGLE = register(VILLAGER_POST_JUNGLE_ID, VillagerPostBlock::new, createVilliagerPostSettings());
+	public static final Item VILLAGER_POST_JUNGLE_ITEM = register(VILLAGER_POST_JUNGLE_ID, ItemGroups.FUNCTIONAL, VILLAGER_POST_JUNGLE, new Item.Settings());
+
+	public static final Identifier VILLAGER_POST_MANGROVE_ID = Identifier.of(VillagerLock.MOD_ID, "villagerpost_mangrove");
+	public static final Block VILLAGER_POST_MANGROVE = register(VILLAGER_POST_MANGROVE_ID, VillagerPostBlock::new, createVilliagerPostSettings());
+	public static final Item VILLAGER_POST_MANGROVE_ITEM = register(VILLAGER_POST_MANGROVE_ID, ItemGroups.FUNCTIONAL, VILLAGER_POST_MANGROVE, new Item.Settings());
+
+	public static final Identifier VILLAGER_POST_PALE_OAK_ID = Identifier.of(VillagerLock.MOD_ID, "villagerpost_pale_oak");
+	public static final Block VILLAGER_POST_PALE_OAK = register(VILLAGER_POST_PALE_OAK_ID, VillagerPostBlock::new, createVilliagerPostSettings());
+	public static final Item VILLAGER_POST_PALE_OAK_ITEM = register(VILLAGER_POST_PALE_OAK_ID, ItemGroups.FUNCTIONAL, VILLAGER_POST_PALE_OAK, new Item.Settings());
+
+	public static final Identifier VILLAGER_POST_SPRUCE_ID = Identifier.of(VillagerLock.MOD_ID, "villagerpost_spruce");
+	public static final Block VILLAGER_POST_SPRUCE = register(VILLAGER_POST_SPRUCE_ID, VillagerPostBlock::new, createVilliagerPostSettings());
+	public static final Item VILLAGER_POST_SPRUCE_ITEM = register(VILLAGER_POST_SPRUCE_ID, ItemGroups.FUNCTIONAL, VILLAGER_POST_SPRUCE, new Item.Settings());
+
+	public static final Identifier VILLAGER_POST_WARPED_ID = Identifier.of(VillagerLock.MOD_ID, "villagerpost_warped");
+	public static final Block VILLAGER_POST_WARPED = register(VILLAGER_POST_WARPED_ID, VillagerPostBlock::new, createVilliagerPostSettings());
+	public static final Item VILLAGER_POST_WARPED_ITEM = register(VILLAGER_POST_WARPED_ID, ItemGroups.FUNCTIONAL, VILLAGER_POST_WARPED, new Item.Settings());
 
 	private static AbstractBlock.Settings createVilliagerPostSettings() {
 		return AbstractBlock.Settings.create()
@@ -60,5 +101,22 @@ public class ModBlocks {
 	public static void initialize() {
 	}
 
-	public static final BlockEntityType<VillagerPostBlockEntity> VILLAGER_POST_ENTITY = Registry.register(Registries.BLOCK_ENTITY_TYPE, VILLAGER_POST_ID, FabricBlockEntityTypeBuilder.create(VillagerPostBlockEntity::new, VILLAGER_POST).build());
+	public static final BlockEntityType<VillagerPostBlockEntity> VILLAGER_POST_ENTITY = Registry.register(
+			Registries.BLOCK_ENTITY_TYPE,
+			VILLAGER_POST_ENTITY_ID,
+			FabricBlockEntityTypeBuilder.create(
+					VillagerPostBlockEntity::new,
+					VILLAGER_POST,
+					VILLAGER_POST_ACACIA,
+					VILLAGER_POST_BIRCH,
+					VILLAGER_POST_CHERRY,
+					VILLAGER_POST_CRIMSON,
+					VILLAGER_POST_DARK_OAK,
+					VILLAGER_POST_JUNGLE,
+					VILLAGER_POST_MANGROVE,
+					VILLAGER_POST_PALE_OAK,
+					VILLAGER_POST_SPRUCE,
+					VILLAGER_POST_WARPED
+			).build()
+	);
 }
